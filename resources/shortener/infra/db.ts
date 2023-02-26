238f5a0f42dae115d017851d37d77b6dae14f481
Daniel Szekely <danszekely@gmail.com>
2023-02-26 16:00:41 -0500

    remove unused db code

--- resources/shortener/infra/db.ts
+++ resources/shortener/infra/db.ts
@@ -1,21 +1,4 @@
-import { Pool, PoolClient } from "pg"
-
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-
-let pool: Pool;
-
-export function getDb(): Promise<PoolClient> {
-  if (!pool) {
-    pool = new Pool({
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "postgres",
-      password: process.env.DB_PASSWORD || "postgres",
-      database: process.env.DATABASE || "url" // Can be replaced for testing purposesu
-    })
-  }
-
-  return pool.connect()
-}
 
 let docClient: DynamoDB;
 
@@ -30,8 +13,4 @@ export async function getClient(): Promise<DynamoDB> {
 
   return docClient;
 
-}
-
-export async function close() {
-  return pool.end()
 }
