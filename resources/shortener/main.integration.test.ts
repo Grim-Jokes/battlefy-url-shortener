c80a14bd89ab5fea843437ddc9c50a554ccde269
Daniel Szekely <danszekely@gmail.com>
2023-02-27 10:07:44 -0500

    add test for actual lambda handler
    This will ensure that everything is wired up correctly without
    needing to constantly deploy the handler and potentially incurring
    extra costs.

---
+++ resources/shortener/main.integration.test.ts
@@ -0,0 +1,37 @@
+const main = require('./main');
+
+describe("test", () => {
+
+  it("Should save a url", async () => {
+
+    const { body, statusCode } = await main.main({
+      httpMethod: "POST",
+      body: JSON.stringify({ url: "http://www.google.ca" })
+    });
+
+    const { shortLink } = JSON.parse(body);
+
+    expect(statusCode).toBe(201)
+
+    const result = await new Promise((success, fail) => dynamoClient.getItem({
+      TableName: process.env.DYNAMO_TABLE_NAME || "url",
+      Key: {
+        'shortUrl': { S: shortLink }
+      },
+      ProjectionExpression: "longUrl"
+    }, (err, data) => {
+      if (err) {
+        return fail(err)
+      }
+
+      if (!data.Item) {
+        return success(null)
+      }
+
+      success(data.Item?.longUrl.S as string)
+    }));
+
+    expect(result).toEqual("http://www.google.ca")
+  })
+
+})
